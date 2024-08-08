3142d8774631d71f1480136e23c102aebe46961d
John <103416957+jeonlei-01@users.noreply.github.com>
2024-08-08 23:36:10 +0800

    New codes

---
+++ send_email.php
@@ -0,0 +1,42 @@
+<?php
+require './vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'johannaraymendoza@gmail.com';
+        $mail->Password   = 'pdonqajmdvwfuuao';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('johannaraymendoza@gmail.com'); // Add a recipient
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = nl2br($message);  // Converts newlines to <br> tags in HTML
+        $mail->AltBody = $message;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request.";
+}
