2cb434fd269c229a9479c2492b017e7eb783aa16
john <johannaraymendoza@gmail.com>
2024-08-21 02:14:26 +0800

    new updates

---
+++ src/views/ArtPortfolio.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <main class="h-screen">
+        <h1 class="text-3xl font-bold underline">Home arts</h1>
+    </main>
+</template>
---
+++ src/views/WebsitePortfolio.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HeaderComponent from '../components/HeaderComponent.vue'
+import ContentComponent from '../components/ContentComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
+const buttonOne = 'Home'
+const buttonTwo = 'About'
+const buttonThree = 'Contact'
+</script>
+
+<template>
+  <HeaderComponent :button1="buttonOne" :button2="buttonTwo" :button3="buttonThree" />
+  <ContentComponent />
+  <FooterComponent />
+</template>
