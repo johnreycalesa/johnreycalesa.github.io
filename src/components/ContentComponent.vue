dd71c3524fa962c5fdc66dcff79822033a4b4cd2
john <johannaraymendoza@gmail.com>
2024-08-27 23:45:40 +0800

    new updates

--- src/components/ContentComponent.vue
+++ src/components/ContentComponent.vue
@@ -4,5 +4,6 @@
 <template>
     <main>
         <h1 class="text-3xl font-bold underline">Content</h1>
+        <a href="/art" target="_blank" rel="noopener noreferrer">Art</a>
     </main>
 </template>
--- src/views/ArtPortfolio.vue
+++ src/views/ArtPortfolio.vue
@@ -4,5 +4,6 @@
 <template>
     <main class="h-screen">
         <h1 class="text-3xl font-bold underline">Home arts</h1>
+        <a href="/" rel="noopener noreferrer">Home</a>
     </main>
 </template>
