c4bf64204f877b3697749cd0b00b5274d31794be
John <johannaraymendoza@gmail.com>
2024-11-29 11:02:21 +0800

    Updates

---
+++ assets/ArtPortfolio-De4N3gcI.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as s,a as e}from"./index-8qllLCD-.js";const r={},n={class:"h-screen"},c=e("h1",{class:"text-3xl font-bold underline"},"Home arts",-1),a=e("a",{href:"/",rel:"noopener noreferrer"},"Home",-1),_=[c,a];function l(d,f){return s(),t("main",n,_)}const h=o(r,[["render",l]]);export{h as default};
