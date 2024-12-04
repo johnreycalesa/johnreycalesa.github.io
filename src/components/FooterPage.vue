d0fd1d043035d61a78fa53f08a527deaff96f1a7
John <johannaraymendoza@gmail.com>
2024-12-05 00:22:11 +0800

    .

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -23,43 +23,33 @@ onMounted(() => {
 
 <template>
   <footer
-    class="text-black p-4 md:py-28 bg-gray-300 dark:bg-[var(--vt-c-primary)] footer-animate opacity-0 translate-y-10"
-  >
+    class="text-black p-4 md:pt-24 md:pb-12 bg-gray-300 dark:bg-[var(--vt-c-primary)] footer-animate opacity-0 translate-y-10">
     <div class="container mx-auto wrapper">
       <div class="flex flex-wrap justify-center items-center md:justify-around">
-        <div
-          class="w-full md:w-1/2 p-4 mb-4 md:mb-0 border-2 rounded-2xl border-black bg-white dark:border-quaternary"
-        >
+        <div class="w-full md:w-1/2 p-4 mb-4 md:mb-0 border-2 rounded-2xl border-black bg-white dark:border-quaternary">
           <h2 class="text-xl font-bold mb-4 dark:text-black">Contact us</h2>
           <form method="post" class="flex flex-col gap-4">
             <div class="">
               <label class="block mb-2 text-sm font-bold dark:text-black">Name</label>
               <input
                 class="form-input w-full px-2 border-2 border-black h-12 rounded-xl bg-gray-200 dark:border-quaternary"
-                type="text"
-                name="name"
-              />
+                type="text" name="name" />
             </div>
             <div class="">
               <label class="block mb-2 text-sm font-bold dark:text-black">Email</label>
               <input
                 class="form-input w-full px-2 border-2 border-black h-12 rounded-xl bg-gray-200 dark:border-quaternary"
-                type="email"
-                name="email"
-              />
+                type="email" name="email" />
             </div>
             <div class="">
               <label class="block mb-2 text-sm font-bold dark:text-black">Message</label>
               <textarea
                 class="form-input w-full px-2 border-2 border-black rounded-xl bg-gray-200 dark:border-quaternary"
-                name="message"
-                rows="6"
-              ></textarea>
+                name="message" rows="6"></textarea>
             </div>
             <button
               class="bg-secondary text-black px-4 py-2 rounded w-full border-2 hover:bg-blue-600 dark:bg-[var(--vt-c-primary)] dark:text-white dark:border-quaternary"
-              type="submit"
-            >
+              type="submit">
               Send
             </button>
           </form>
@@ -98,6 +88,11 @@ onMounted(() => {
         </div>
       </div>
     </div>
+    <div class="mt-4 w-full flex justify-end">
+      <a href='https://ko-fi.com/U6U0124XJF' target='_blank'><img height='55' style='border:0px;height:55px;'
+          src='/photos/kofi.gif' border='0' alt='Buy Me a Coffee at ko-fi.com'
+          class="icons bg-white rounded-full p-2" /></a>
+    </div>
   </footer>
 </template>
 
@@ -125,6 +120,10 @@ onMounted(() => {
 }
 
 li.icons:hover {
+  transform: scale(1.25);
+}
+
+a:hover {
   transform: scale(1.25);
 }
 </style>
